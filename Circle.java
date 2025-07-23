2904a6796f1aac9da08ea406022957d1a3e29aa2
ngkaizhe123 <kaizhe8229@gmail.com>
2025-07-23 15:33:16 +0800

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,24 @@
+public class Circle{
+    private double radius;
+    
+    public Circle(){
+        this.radius = 1.0;
+    }
+    
+    public Circle(double radius){
+        this.radius = radius;
+    }
+    
+    public double getRadius(){
+        return radius;
+    }
+    
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+    
+    @Override
+    public String toString(){
+        return super.toString() + "\nRadius: " + this.radius;
+    }
+}
